21fdaff5b3c71e6a77a26a52d398deb6169cac2c
Khaledelabady11 <k7aled.el3bady123@gmail.com>
2022-12-05 06:31:07 +0200

    Updated Database Schema

--- game_controller.py
+++ game_controller.py
@@ -1,37 +1,37 @@
 from db import get_db
 
 
-def insert_game(name, price, rate):
+def insert_game(name,message):
     db = get_db()
     cursor = db.cursor()
-    statement = "INSERT INTO games(name, price, rate) VALUES (?, ?, ?)"
-    cursor.execute(statement, [name, price, rate])
+    statement = "INSERT INTO person(name, message) VALUES (?,?)"
+    cursor.execute(statement, [name, message])
     db.commit()
     return True
 
 
-def update_game(id, name, price, rate):
+def update_game(id, name, image_encodings, message):
     db = get_db()
     cursor = db.cursor()
-    statement = "UPDATE games SET name = ?, price = ?, rate = ? WHERE id = ?"
-    cursor.execute(statement, [name, price, rate, id])
+    statement = "UPDATE person SET name = ?, image_encodings = ?, message = ? WHERE id = ?"
+    cursor.execute(statement, [name, image_encodings, message])
     db.commit()
     return True
 
 
-def delete_game(id):
-    db = get_db()
-    cursor = db.cursor()
-    statement = "DELETE FROM games WHERE id = ?"
-    cursor.execute(statement, [id])
-    db.commit()
-    return True
+# def delete_game(id):
+#     db = get_db()
+#     cursor = db.cursor()
+#     statement = "DELETE FROM games WHERE id = ?"
+#     cursor.execute(statement, [id])
+#     db.commit()
+#     return True
 
 
 def get_by_id(id):
     db = get_db()
     cursor = db.cursor()
-    statement = "SELECT id, name, price, rate FROM games WHERE id = ?"
+    statement = "SELECT id, name, image_encodings, message FROM person WHERE id = ?"
     cursor.execute(statement, [id])
     return cursor.fetchone()
 
@@ -39,6 +39,6 @@ def get_by_id(id):
 def get_games():
     db = get_db()
     cursor = db.cursor()
-    query = "SELECT id, name, price, rate FROM games"
+    query = "SELECT id, name, message FROM person"
     cursor.execute(query)
     return cursor.fetchall()
